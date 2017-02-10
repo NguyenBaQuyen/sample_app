fb1c54680221f0d33295eafff06cf79a1eacc154
Nguyen Ba Quyen <nguyenbaquyen95@gmail.com>
2017-02-10 09:46:10 +0700

    Make a basis User model (including sercure password)

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
+  validates :password, presence: true, length: {maximum: 6}
+  has_secure_password
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
+end
