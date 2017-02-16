706bfa5630bfd62de5ee40290b7298e76d7321b7
Nguyen Ba Quyen <nguyenbaquyen95@gmail.com>
2017-02-16 14:45:08 +0700

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,32 @@
 class User < ApplicationRecord
   enum gender: [:male, :female]
+  attr_accessor :remember_token
   before_save :downcase_email
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
-  validates :password, presence: true, length: {maximum: 6}
+  validates :password, presence: true, length: {maximum: 6}, allow_nil: true
   validates :gender, presence: true
   has_secure_password
+
+  def forget
+    update_attributes :remember_digest, nil
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
   private
   def downcase_email
     self.email = email.downcase
