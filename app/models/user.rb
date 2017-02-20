ac1419cebc80ca1d5ea0152b6ad1ee11cb752fc8
Nguyen Ba Quyen <nguyenbaquyen95@gmail.com>
2017-02-20 15:18:30 +0700

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   enum gender: [:male, :female]
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
@@ -31,6 +32,21 @@ class User < ApplicationRecord
     self == user
   end
 
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def activate
+    update_attributes activated: true
+    update_attributes activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
   def downcase_email
     self.email = email.downcase
@@ -42,4 +58,8 @@ class User < ApplicationRecord
     BCrypt::Password.create string, cost: cost
   end
 
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
